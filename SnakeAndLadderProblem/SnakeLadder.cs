5de0154cd44bec5082acf1bef4623912f5e39c97
7007singh <shw12021982@gmail.com>
2023-01-24 23:43:57 +0530

    [Shweta]Added: Player Rolls a Die

--- SnakeAndLadderProblem/SnakeLadder.cs
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -10,8 +10,12 @@ namespace SnakeAndLadderProblem
     {
         public int position = 0;
         public int player;
+
+        Random random = new Random();
         public void Start()
         { 
+            int dieNumber = random.Next(1, 7);
+            Console.WriteLine("dice Number: " + dieNumber);
             player = position;
             Console.WriteLine("Position of Player: " + position);
         }
